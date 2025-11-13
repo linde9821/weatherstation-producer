b3e4363d9b23387cd4a29899a7145b5cbf239ea4
Moritz Lindner <lindner197@gmail.com>
2025-11-13 17:48:44 +0100

    add java

--- NeoProducer/update.sh
+++ NeoProducer/update.sh
@@ -2,6 +2,9 @@ git pull
 
 sudo systemctl stop homekit-sensor
 
+export JAVA_HOME=/home/pi/.sdkman/candidates/java/current
+export PATH=/home/pi/.sdkman/candidates/java/current/bin:$PATH
+
 ./gradlew installDist
 
 sudo systemctl start homekit-sensor
